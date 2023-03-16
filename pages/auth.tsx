03290fa3fdec102ce487d1ae94fda503c40acd2e
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-03-15 21:53:42 -0500

    feat: implement Github Oauth

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -5,6 +5,9 @@ import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useCallback, useState } from 'react';
 
+import { FaGithub } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+
 interface ICredentials {
   email: string;
   name: string;
@@ -116,6 +119,17 @@ const Auth = () => {
               >
                 {variant}
               </button>
+              <div className="flex flex-row-items-center gap-4 mt-8 justify-center">
+                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                  <FcGoogle size={30} />
+                </div>
+                <div
+                  onClick={() => signIn('github', { callbackUrl: '/' })}
+                  className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                >
+                  <FaGithub size={30} />
+                </div>
+              </div>
             </form>
             <p className="text-neutral-500 mt-12">
               {variant === 'login'
