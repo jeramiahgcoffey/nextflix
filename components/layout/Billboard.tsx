cff1164150f49e03a83d26e63c347fca85a6d779
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-03-19 01:29:16 -0500

    feat: implement InfoModal

--- components/layout/Billboard.tsx
+++ components/layout/Billboard.tsx
@@ -1,9 +1,11 @@
 import useBillboard from '@/hooks/useBillboard';
+import useInfoModal from '@/hooks/useInfoModal';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import PlayButton from '../inputs/PlayButton';
 
 const Billboard = () => {
   const { data: movie } = useBillboard();
+  const { openModal } = useInfoModal();
 
   return (
     <div className="relative h-[56.25vw]">
@@ -25,6 +27,7 @@ const Billboard = () => {
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
           <PlayButton movieId={movie?.id} />
           <button
+            onClick={() => openModal(movie?.id)}
             className="
         bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition
       "
